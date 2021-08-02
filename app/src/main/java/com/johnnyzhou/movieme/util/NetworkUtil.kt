bde8fdbf49d5774aa5f0ae3cfc3b07f2dfc860dc
JohnnyZhou <johnny.zhou@gmail.com>
2021-08-03 00:26:50 +1000

    asdf

---
+++ app/src/main/java/com/johnnyzhou/movieme/util/NetworkUtil.kt
@@ -0,0 +1,12 @@
+package com.johnnyzhou.movieme.util
+
+class NetworkUtil {
+    companion object {
+        const val API_KEY = "42af06c42da2647304c1037af14aea64"
+        const val API_URL = "https://api.themoviedb.org/3/"
+
+        const val PROFILE_BASE_PATH = "http://image.tmdb.org/t/p/w185"
+        const val BACKDROP_BASE_PATH = "http://image.tmdb.org/t/p/w780"
+        const val POSTER_BASE_PATH = "http://image.tmdb.org/t/p/w185"
+    }
+}
